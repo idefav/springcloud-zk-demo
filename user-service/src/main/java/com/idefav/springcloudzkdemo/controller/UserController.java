b2a2325a911923624f64ce49a049fe22dd57d335
wuzishu <wzs2009_hi@126.com>
2021-10-26 08:54:24 +0800

    update

--- user-service/src/main/java/com/idefav/springcloudzkdemo/controller/UserController.java
+++ user-service/src/main/java/com/idefav/springcloudzkdemo/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.idefav.springcloudzkdemo.controller;
 
+import com.alibaba.csp.sentinel.slots.adaptive.AdaptiveRuleManager;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -17,12 +24,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @PostConstruct
+    public void init() {
+        AdaptiveRuleManager.setEnabled(true);
+    }
+
+    AtomicInteger counter = new AtomicInteger(0);
+
     @Autowired
     private ServiceInstance serviceInstance;
 
 
     @GetMapping("/info/{userName}")
-    public String userName(@PathVariable("userName") String userName) {
-        return String.format("[%s:%s]%s",serviceInstance.getHost(),serviceInstance.getPort(), userName);
+    public String userName(@PathVariable("userName") String userName, HttpServletRequest request) throws InterruptedException {
+//        Thread.sleep(1000);
+        return String.format("[%s:%s]%s", serviceInstance.getHost(), serviceInstance.getPort(), userName);
+    }
+
+    @GetMapping("/cpu/{n}")
+    public Long cpu(@PathVariable("n") Long number) {
+        return fibonacci(number);
+    }
+
+    @GetMapping("/io/{n}")
+    public String io(@PathVariable("n") Long time) throws InterruptedException {
+        counter.incrementAndGet();
+        if (counter.get() <= 10) {
+            Thread.sleep(100);
+        } else {
+            Thread.sleep(time);
+        }
+
+        return "ok";
+    }
+
+    public long fibonacci(long number) {
+        if ((number == 0) || (number == 1))
+            return number;
+        else
+            return fibonacci(number - 1) + fibonacci(number - 2);
     }
 }
